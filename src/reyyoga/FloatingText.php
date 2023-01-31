3b50f9968f328e059728b06775fc2c4a1007fa54
AlvinHere <106876386+AlvinHere@users.noreply.github.com>
2023-01-31 13:20:02 +0800

    works

--- src/reyyoga/FloatingText.php
+++ src/reyyoga/FloatingText.php
@@ -2,28 +2,40 @@
 
 namespace reyyoga;
 
-use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\world\particle\FloatingTextParticle;
+
 use pocketmine\math\Vector3;
 
 class FloatingText extends FloatingTextParticle{
 
     public function __construct(Main $pl, Vector3 $pos){
+
      parent::__construct($pos, "", "");
-     $this->level = $pl->getServer()->getDefaultLevel();
+
+     $this->level = $pl->getServer()->getWorldManager()->getDefaultWorld();
+
      $this->pos = $pos;
+
     }
 
     public function setText(string $text):void{
+
      $this->text = $text;
+
      $this->update();
+
     }
 
     public function setTitle(string $title):void{
+
      $this->title = $title;
+
     }
 
     public function update():void{
-     $this->level->addParticle($this);
+
+     $this->level->addParticle($this->pos, $this);
+
     }
 
 }
